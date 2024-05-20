ce4a38fb683ad0b0168f57c4d1390b6f95014f49
Apurv <destinationiscoming@gmail.com>
2024-05-20 16:35:23 +0530

    all done

---
+++ Strings/Password.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h> 
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ //priority in (),*,/ should be kept in consideration.
+ //can also use a+b>2*c , don't need to use double;
+ 
+// can use sum of integers while checking the multiple integers value
+// be aware of integer divisions while needing the double value, cast them as doubles,floats
+ 
+//integer reverse
+// int n;
+// cin>>n;
+// bool p{false};
+// while(n>9)
+// {
+//     int k=n%10;
+//     n/=10;
+//     if(k>0)
+//     p=true;
+//     if(p)
+//     cout<<k;
+// }
+// cout<<n<<endl;
+// int r{};
+// while(n>0)
+// {
+//     int k=n%10;
+//     n/=10;
+//     r=r*10+k;
+// }
+// cout<<r<<endl;
+//string n,k;
+//cin>>n;
+//for(int i=(n.size()-1); i>=0; i--)
+//k+=n[i];
+//cout<<stoi(k)<<endl;:
+
+
+void solve()
+{
+    string s;
+    cin>>s;
+    int l=s.size();
+    string s2={'@','#','%','&','?'};
+    bool lc{false},up{false},d{false},ch{false};
+    for(int i=0; i<s.size(); i++)
+    {
+        // if(s[i]>=97 && s[i]<=122)
+        // lc=true;
+        // else if(s[i]>=65 && s[i]<=90 && i>=1 && i<=l-2)
+        // up=true;
+        // else if(s[i]>=48 && s[i]<=57 && i>=1 && i<=l-2)
+        // d=true;
+        // else if((s[i]=='@' || s[i]=='#' || s[i]=='%' || s[i]=='&' || s[i]=='?') && i>=1 && i<=l-2)
+        // ch=true;
+
+        // if(s[i]>='A' && s[i]<='Z' && i>=1 && i<=l-2)
+        // up=true;
+        // else if(s[i]>='a' && s[i]<='z')
+        // lc=true;
+        // else if(s[i]>='0' && s[i]<='9' && i>=1 && i<=l-2)
+        // d=true;
+        // else if(s2.find(s[i])!=string:: npos && i>=1 && i<=l-2)
+        // ch=true;
+
+        if(isupper(s[i]) && i>=1 && i<=l-2)
+        up=true;
+        else if(islower(s[i]) )
+        lc=true;
+        else if(isdigit(s[i]) && i>=1 && i<=l-2)
+        d=true;
+        else if(s2.find(s[i])!=string:: npos && i>=1 && i<=l-2)
+        ch=true;
+    }
+    if(lc && up && d && ch && l>=10)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
+int main()
+  {
+  fast_cin();
+  ll t;
+  cin >> t;
+  for(int i=0;i<t;i++)
+// cout << "Case #" << it+1 << ": ";
+  {
+  solve();
+  }
+  return 0;
+}
---
+++ Time Complexity/Maximize_the_Minimum.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h> 
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ //priority in (),*,/ should be kept in consideration.
+ //can also use a+b>2*c , don't need to use double;
+ 
+// can use sum of integers while checking the multiple integers value
+// be aware of integer divisions while needing the double value, cast them as doubles,floats
+ 
+//integer reverse
+// int n;
+// cin>>n;
+// bool p{false};
+// while(n>9)
+// {
+//     int k=n%10;
+//     n/=10;
+//     if(k>0)
+//     p=true;
+//     if(p)
+//     cout<<k;
+// }
+// cout<<n<<endl;
+// int r{};
+// while(n>0)
+// {
+//     int k=n%10;
+//     n/=10;
+//     r=r*10+k;
+// }
+// cout<<r<<endl;
+//string n,k;
+//cin>>n;
+//for(int i=(n.size()-1); i>=0; i--)
+//k+=n[i];
+//cout<<stoi(k)<<endl;:
+
+
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    ll a[n];
+    for(int i=0 ;i<n; i++)
+    cin>>a[i];
+    if(k>=(n-1))
+    cout<<*max_element(a,a+n)<<endl;
+    else
+    {
+       sort(a,a+n);
+       cout<<a[k]<<endl;
+    }
+}
+int main()
+  {
+  fast_cin();
+  ll t;
+  cin >> t;
+  for(int i=0;i<t;i++)
+// cout << "Case #" << it+1 << ": ";
+  {
+  solve();
+  }
+  return 0;
+}
